f408e82c84d254c75fbcb292cea51d3ec112e60c
Carlos Espinal <cespinalb@gmail.com>
2023-03-04 16:27:47 -0600

    Prueba de commit

---
+++ src/pfinalp1_cespinal_aieong/PFinalP1_CEspinal_AIeong.java
@@ -0,0 +1,13 @@
+
+package pfinalp1_cespinal_aieong;
+
+
+public class PFinalP1_CEspinal_AIeong {
+
+
+    public static void main(String[] args) {
+        mprincipal m = new mprincipal();
+        m.setVisible(true);
+    }
+    
+}
